cb8ea6041081bf8c9a6aef2372e64c81bb6de873
stijnimaging <stijn.imaging@gmail.com>
2015-11-25 15:36:49 +0100

    Update COPE_check.sh

--- COPE_check.sh
+++ COPE_check.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # Target directory = run in .feat dir
-DATA=$PWD
 
-cd ${DATA}
+for feat in */ ; do
+	cd $feat
+	echo "<p>FEAT Directory = $feat <br>">> ../FEAT_summary.html
 
-
-	for cope in 1; do
-	
-		echo "	<p>COPE${cope} - zstat1 &nbsp;&nbsp;-&nbsp;&nbsp; C${cope} <br>
-			<a href=cluster_zstat${cope}_std.html><IMG BORDER=0 SRC=rendered_thresh_zstat${cope}.png></a> "
-	done
- >> ..//FEAT_summary.html
+		for cope in {1..11} ; do
+			echo "<p>COPE${cope} - zstat1 &nbsp;&nbsp;-&nbsp;&nbsp; C${cope} <br>
+				<a href=$feat/cluster_zstat${cope}_std.html><IMG BORDER=0 SRC=$feat/rendered_thresh_zstat${cope}.png></a>"
+		done >> ../FEAT_summary.html
+			#append to FEAT_summary.html
+	cd ../
+done
