cf4ed7edb37e51f95b6cbee275f4ad5b34ea712c
Stijn <stijn.imaging@gmail.com>
2017-11-14 16:08:56 +0100

    Add files via upload

--- COPE_check.sh
+++ COPE_check.sh
@@ -2,10 +2,11 @@
 
 # Target directory = run in .feat dir
 
+
 for feat in */ ; do
 	cd $feat
 	echo "<p>FEAT Directory = $feat <br>">> ../FEAT_summary.html
-
+	DATA=$PWD
 		for cope in {1..11} ; do
 			echo "<p>COPE${cope} - zstat1 &nbsp;&nbsp;-&nbsp;&nbsp; C${cope} <br>
 				<a href=$feat/cluster_zstat${cope}_std.html><IMG BORDER=0 SRC=$feat/rendered_thresh_zstat${cope}.png></a>"
